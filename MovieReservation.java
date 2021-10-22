d407ce3d775e091a3da77541f43935b862c1b2d7
kusumkona123 <69755640+kusumkona123@users.noreply.github.com>
2021-10-22 21:02:23 +0530

    Add files via upload

---
+++ MovieReservation.java
@@ -0,0 +1,48 @@
+class MovieReservation implements Runnable
+{
+int ticket=2;
+int t;
+MovieReservation(int t)
+{
+this.t=t;
+}
+public void run()
+{
+String name=Thread.currentThread().getName();
+synchronized(this){
+if(t<=ticket)
+{
+System.out.println(name+"Ticket booked");
+ticket=ticket-1;
+}
+else
+{
+System.out.println(name+"ticket not booked");
+}
+try{
+Thread.sleep(1500);
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+}
+}
+}
+class Synchronization
+{
+public static void main(String args[])
+{
+MovieReservation m=new MovieReservation(1);
+Thread t1=new Thread(m);
+t1.setName("ravi");
+Thread t2=new Thread(m);
+
+t2.setName("kalyan");
+Thread t3=new Thread(m);
+t3.setName("nina");
+t1.start();
+t2.start();
+t3.start();
+}
+}
